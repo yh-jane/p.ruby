ce61a5c3ae9a21754ca78df80d45005ef840bf15
Jane H. Yamamoto <82067292+yh-jane@users.noreply.github.com>
2025-04-06 04:01:43 +0000

    att

---
+++ ruby.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome: "
+nome = gets.chomp #método de entrada
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+#saida final
+puts "Olá, #{nome} #{sobrenome}! Sua idade é #{idade}."
